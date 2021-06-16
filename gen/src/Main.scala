630d840e7bac547a10c8cede4e76591109dca4e7
dmitry-worker <dzenmassta@gmail.com>
2021-06-16 13:09:58 +0300

    removed old trash

--- gen/src/Main.scala
+++ gen/src/Main.scala
@@ -2,26 +2,11 @@ package gen
 
 import SchemaMacros._
 
-case class GetBlockByIndexRequest(i: Int)
-
-case class GetBlockByHashRequest(hash: Int)
-
-case class Block( 
-  hash: String,
-  index: Int,
-  someShit: Option[String]
-)
-
-case class BlockResponse(block: Block)
-
 // Just invoke `mill gen.run`
 // Then check the result on the https://playground.open-rpc.org/
 object Main {
 
   def main(args:Array[String]):Unit = {
-
-    implicit val reqGen:Agnostic.Repr[GetBlockByIndexRequest] = gen[GetBlockByIndexRequest]
-    implicit val respGen = gen[BlockResponse]
 
     // this is the ONLY thing required to define a schema
     // TODO: provide examples for each method
